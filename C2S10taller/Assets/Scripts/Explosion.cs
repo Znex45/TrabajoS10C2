37963f27bfaaa81bf73e801305d2a786c8f63aca
ElamGomez20 <elam.gomez@uao.edu.co>
2025-10-04 00:44:15 -0500

    Muerte por Caida
    Añadí una caida y reinicio de nivel si se cae de las plataformas.

--- C2S10taller/Assets/Scripts/Explosion.cs
+++ C2S10taller/Assets/Scripts/Explosion.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] private float tiempoVida = 0.5f; // tiempo antes de destruir
+    [SerializeField] private float tiempoVida = 0.5f; 
 
     private void Start()
     {
-        // Se destruye automáticamente después de "tiempoVida" segundos
+        // Se destruye automáticamente después de segundos
         Destroy(gameObject, tiempoVida);
     }
 }
